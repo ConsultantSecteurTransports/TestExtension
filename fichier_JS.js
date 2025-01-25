e7637a80865dd9e05aa5ac9bac1a4597dfa3de12
ConsultantSecteurTransports <imad.bouhmouch@ratp.fr>
2025-01-25 20:44:38 +0100

    Update fichier_JS.js

--- fichier_JS.js
+++ fichier_JS.js
@@ -9,30 +9,32 @@
                 if (data.data === "open_menu") {  //Attention, il faut recupere data dans date !
                     console.log("Commande reçue : Création du menu.");
 
-                    // Configuration du menu principal
-                    const mainMenuObject = {
-                        title: "Supports de formation",
-                        icon: "https://consultantsecteurtransports.github.io/TestExtension/Logos/Logo-formation.png",
-                        command: "main_nav_menu_clicked",
-                        subMenus: [
-                            {
-                                title: "Videos",
-                                icon: "https://consultantsecteurtransports.github.io/TestExtension/Logos/Logo-video.png",
-                                command: "submenu_1_clicked",
-                            },
-                            {
-                                title: "PDF",
-                                icon: "https://consultantsecteurtransports.github.io/TestExtension/Logos/Logo-pdf.png",
-                                command: "submenu_2_clicked",
-                            },
-                        ],
-                    };
+
 
                 }
             }
         });
 
-                // Mise à jour du menu via l'API
+        // Configuration du menu principal
+        const mainMenuObject = {
+            title: "Supports de formation",
+            icon: "https://consultantsecteurtransports.github.io/TestExtension/Logos/Logo-formation.png",
+            command: "main_nav_menu_clicked",
+            subMenus: [
+                {
+                    title: "Videos",
+                    icon: "https://consultantsecteurtransports.github.io/TestExtension/Logos/Logo-video.png",
+                    command: "submenu_1_clicked",
+                },
+                {
+                    title: "PDF",
+                    icon: "https://consultantsecteurtransports.github.io/TestExtension/Logos/Logo-pdf.png",
+                    command: "submenu_2_clicked",
+                },
+            ],
+        };
+        
+        // Mise à jour du menu via l'API
         API.ui.setMenu(mainMenuObject);
         console.log("Menu principal configuré avec succès.");
 
@@ -45,6 +47,7 @@
         console.log("Jeton d'accès récupéré :", accessToken);
 
         console.log("Connexion avec l'API réussie !");
+        
     } catch (error) {
         console.error("Erreur lors de la connexion à l'API Trimble Connect :", error);
     }
