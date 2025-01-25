bc99715fbc275ca11e564fc961ad4db9b62b4a5a
ConsultantSecteurTransports <imad.bouhmouch@ratp.fr>
2025-01-25 13:35:29 +0100

    Update videoJS.js

--- videoJS.js
+++ videoJS.js
@@ -1,17 +1,13 @@
 (async () => {
     try {
-        // Utilisez TrimbleConnectWorkspace.connect pour établir une connexion
-        const API = await TrimbleConnectWorkspace.connect(window.parent);
+        var API = await TrimbleConnectWorkspace.connect(window.parent, (event, data) => {
+            console.log("Event: ", event, data);
+          });
+                
+          API.project.getProject().then(project => {
+            console.log(project); // Trimble Connect project details
 
-        console.log("API Trimble Connect récupérée :", API);
-
-        // Exemple : Récupérer les détails du projet
-        const project = await API.project.getProject();
-        console.log("Détails du projet :", project);
-
-        // Exemple : Interaction avec le visualiseur 3D
-        const viewer = await API.viewer.getViewer();
-        console.log("Visualiseur récupéré :", viewer);
+        
     } catch (error) {
         console.error("Erreur lors de la connexion à l'API Trimble Connect :", error);
     }
