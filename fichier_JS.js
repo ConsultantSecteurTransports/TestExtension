63c359d381c8187e4c53e5151bc4a1635a03cb90
ConsultantSecteurTransports <imad.bouhmouch@ratp.fr>
2025-01-25 15:23:29 +0100

    Update fichier_JS.js

--- fichier_JS.js
+++ fichier_JS.js
@@ -6,7 +6,7 @@
 
             // Gestion des événements de commande
             if (event === "command") {
-                if (data.command === "open_video_button") {
+                if (data.command === "open_menu") {
                     console.log("Commande reçue : Création du menu.");
 
                     // Configuration du menu principal
